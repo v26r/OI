a07c12a3de981a475130adeccc5e18873eb0c84b
Mallepalli <MALLEPR@airproducts.com>
2017-07-03 10:56:59 +0100

    First commit

---
+++ OI.TestFramework/PageObjects/OurPeople.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace OI.TestFramework.PageObjects
+{
+    public class OurPeople
+    {
+        #region Private Variables
+
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+        private const string Url = @"https://www.octopusinvestments.com/adviser/about-us/our-people";
+        private const string TitleText = "Our people | Adviser | Octopus Investments";
+        private const string NotFoundMessage = "No results found";
+
+        #endregion Private Variables
+
+        #region Page Elements
+
+        [FindsBy(How = How.Id, Using = "myAdviser")]
+        public IWebElement Modal { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "pink")]
+        public IWebElement BtnContinue { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[3]/div/div/div[1]/div[2]/div[1]/div/div/input")]
+        public IWebElement SearchBox { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "selectOne")]
+        public IWebElement SortBy { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "selectTwo")]
+        public IWebElement Team { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "effect-3")]
+        public IWebElement SearchResults { get; set; }
+        
+
+        #endregion
+
+        #region Constructor
+        public OurPeople(IWebDriver browser)
+        {
+            _driver = browser;
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+        }
+
+        #endregion
+
+        #region Page Actions and Helper methods
+
+        /// <summary>
+        /// Navigates to the url and initializes the elements in page objects
+        /// </summary>
+        public void Open()
+        {
+            //Navigate to Our people page
+            _driver.Navigate().GoToUrl(Url);
+
+            //Initialize the our people page elements in page objects
+            PageFactory.InitElements(_driver, this);
+        }
+
+        /// <summary>
+        /// Compares the expected page title with actual page title and returns true or false
+        /// </summary>
+        /// <returns></returns>
+        public bool IsAt()
+        {
+            return _driver.Title == TitleText;
+        }
+
+        /// <summary>
+        /// Accepts the notification and click on continue button
+        /// </summary>
+        public void ClickContinue()
+        {
+            //Wait for notification Modal to load
+            WaitForVisibilityById(Modal);
+
+            //Click on the continue button
+            Actions actions = new Actions(_driver);
+            actions.MoveToElement(BtnContinue).Click().Perform();
+        }
+
+        /// <summary>
+        /// Searches the team members by using the name entered in the search box and return the no. of matching records
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>count</returns>
+        public int SearchByName(string name)
+        {
+            //Wait for Search Box to load
+            WaitForVisibilityByXpath("/html/body/div[3]/div/div/div[1]/div[2]/div[1]/div/div/input");
+
+            // Search for the given person name by entering in the searchbox
+            SearchBox.Click();
+            SearchBox.Clear();
+            SearchBox.SendKeys(name);
+            SearchBox.SendKeys(Keys.Return);
+
+            // Get the list of persons from Results
+            IList<IWebElement> persons = SearchResults.FindElements(By.TagName("a"));
+
+            // Return the no of persons matching to the given name
+            return persons.Count(a => a.Text.ToLower().Contains(name.ToLower()));
+        }
+
+        /// <summary>
+        /// Searches the team members by using the name not in the list and compare the not found message is same as the expected
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>count</returns>
+        public bool SearchByUnknownName(string name)
+        {
+           // Search for the given person name by entering in the searchbox
+            SearchBox.Click();
+            SearchBox.Clear();
+            SearchBox.SendKeys(name);
+            SearchBox.SendKeys(Keys.Return);
+
+            // Get the list of paragraph elements from Results
+            IList<IWebElement> elements = SearchResults.FindElements(By.TagName("p"));
+
+            // Return the no of elements matching to the given not found message
+            return elements.Any(a => a.Text.Contains(NotFoundMessage));
+        }
+        
+        /// <summary>
+        /// Waits for the element to load based on the given element id
+        /// </summary>
+        /// <param name="element"></param>
+        private void WaitForVisibilityById(IWebElement element)
+        {
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.Id(element.GetAttribute("id"))));
+        }
+
+        /// <summary>
+        /// Waits for the element to load based on the given element XPath
+        /// </summary>
+        /// <param name="xPath"></param>
+        private void WaitForVisibilityByXpath(string xPath)
+        {
+            _wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(xPath)));
+        }
+
+        #endregion
+    }
+}
---
+++ OI.Tests/OurPeopleTestCases.cs
@@ -0,0 +1,157 @@
+﻿
+using NUnit.Framework;
+using OI.TestFramework.PageObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OI.Tests
+{
+    [TestFixture]
+    public class OurPeopleTestCases
+    {
+        #region Variables
+
+        private static IWebDriver _driver;
+        private static OurPeople _ourPeoplePage;
+
+        #endregion
+
+        #region Initialize
+
+        /// <summary>
+        /// Initialize the one time setup 
+        /// </summary>
+        [OneTimeSetUp]
+        public void SetupTest()
+        {
+            //Settings to maximise the browser
+            var chromeoptions = new ChromeOptions();
+            chromeoptions.AddArguments("start-maximized");
+
+            //Initilaize the driver and browser
+            _driver = new ChromeDriver(chromeoptions);
+            _ourPeoplePage = new OurPeople(_driver);
+
+        }
+
+        #endregion
+
+        #region Tests
+
+        /// <summary>
+        /// Test Case to check if the "Our people" page is opened or not by comparing actual page title 
+        /// with the expected page title
+        /// </summary>
+
+        [Test, Order(1)]
+        public void CanOpenOurPeoplePage()
+        {
+            //Open the Our People Page
+            _ourPeoplePage.Open();
+
+            // Accept the notification to continue
+            _ourPeoplePage.ClickContinue();
+
+            //Assert that page title matches the expected title
+            Assert.IsTrue(_ourPeoplePage.IsAt());
+
+        }
+
+        /// <summary>
+        /// Test Case to check if the team members can be searched by using the search box and this test case also covers the scenario of 
+        /// case insensitive search as well
+        /// </summary>
+
+        [Test, Order(2)]
+        public void CanSearchByName()
+        {
+            //Arrange the input and also the expected count
+            string name = "carla";
+            int expectedCount = 1;
+
+            // Search by given name(should work in both capital or small letters) and return the count of matching records
+            int resultCount = _ourPeoplePage.SearchByName(name);
+
+            //Assert that result is not zero and equals to the expected result
+            Assert.NotZero(resultCount);
+            Assert.AreEqual(resultCount, expectedCount);
+
+        }
+
+
+        /// <summary>
+        /// Test Case to check if the team members can be searched by using the search box and if the user enters name but with either 
+        /// with a space suffix("carla ") or with a space prefix(" carla") then search should return matching records by doing the left trim
+        /// and right trim ( As this usually happens when user copies and pastes in the box)
+        /// </summary>
+
+        [Test, Order(3)]
+        public void CanSearchByNameWithSpacePrefixOrSuffix()
+        {
+            //Arrange the input and also the expected count
+            string name = " carla";
+            int expectedCount = 1;
+
+            // Search by given name(should ignore the left and right spaces and then search for exact word) and return the count of matching records
+            int resultCount = _ourPeoplePage.SearchByName(name);
+
+            //Assert that result is not zero and matches the expected result
+            Assert.NotZero(resultCount);
+            Assert.AreEqual(resultCount, expectedCount);
+
+        }
+
+        /// <summary>
+        /// Test Case to check if the searched team member is not found then it should show the proper message
+        ///  </summary>
+
+        [Test, Order(4)]
+        public void CanGetMessageIfNotFound()
+        {
+            //Arrange the input and also the expected count
+            string name = "suma";
+            
+            //Search the name which is not in the list and check if the message is displayed and also compare it with the expected message
+            bool messageDisplayed = _ourPeoplePage.SearchByUnknownName(name);
+
+            //Assert that message is displayed and matches to the expected "No results found" message
+            Assert.IsTrue(messageDisplayed);
+
+        }
+
+        [Ignore("Work In Progress")]
+        [Test,Order(5)]
+        public void CanSortByDescending()
+        {
+            bool sorted = false;
+
+            //Act
+            _ourPeoplePage.Open();
+            _ourPeoplePage.ClickContinue();
+
+            //To DO: Implement the Sort By Descending method
+            //sorted = _ourPeoplePage.SortByDescending();
+
+            //Assert
+            Assert.IsTrue(sorted);
+
+        }
+
+        #endregion
+
+        #region Tear Down
+
+        /// <summary>
+        /// Closes the browser and driver 
+        /// </summary>
+
+        [OneTimeTearDown]
+        public void TearDownTest()
+        {
+            _driver.Close();
+            _driver.Quit();
+        }
+
+        #endregion
+    }
+}
